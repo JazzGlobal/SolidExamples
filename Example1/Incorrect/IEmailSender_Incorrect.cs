b369932267b2e59dae06b64e515052febf4d931d
chris <waymer147@gmail.com>
2025-12-06 00:22:20 -0500

    additional comment

--- Example1/Incorrect/IEmailSender_Incorrect.cs
+++ Example1/Incorrect/IEmailSender_Incorrect.cs
@@ -11,5 +11,7 @@ public interface IEmailSender_Incorrect
 {
     public string SendEmail(string sender, string receiver, string content);
 
+    // This interface is not correct because it requires a function that should not be in the public contract. And in our example, the consumer never
+    // uses this function. SendEmail validating the input is purely an implementation detail.
     public Email ValidateInput(string sender, string receiver, string content);
 }
